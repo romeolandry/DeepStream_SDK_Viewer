c78efb6f9a37d454908612b4e31864d5b3ce63f2
kamgo-gpu <kamgoche@yahoo.com>
2021-01-06 02:28:24 +0100

    sidebare responsie

--- src/static/javascript/base.js
+++ src/static/javascript/base.js
@@ -7,25 +7,26 @@ document.addEventListener("DOMContentLoaded", () =>{
     const sideContent = document.getElementById("sidebare-id")
     const footerContent = document.getElementById("footer-content-id")
 
+    // toggeled side bar menu with JQuery
     $("#button-toggle-id").click(function(){
         $('.sidebare').toggleClass('sidebare-hidden')
         $('.footer-content').toggleClass('footer-content-hidden')        
     });
 
-    btnToggled.addEventListener('click', (event) =>{
-        // if (sideContent.classList.contains("showed")){
-
-        //     sideContent.classList.remove("sidebare")
-        //     sideContent.classList.add("sidebare-hidden")
-        //     footerContent.classList.add("ml-0")
-        //     sideContent.classList.remove("showed")
-        // }
-        // else{
-        //     sideContent.classList.remove("sidebare-hidden")
-        //     sideContent.classList.add("sidebare")
-        //     footerContent.classList.remove("ml-0")
-        //     sideContent.classList.add("showed")
-        // }
-                        
+    // hauptButton menue sidebare
+    const btnSidebareHeader = document.getElementById("sidebare-btn-header-id")
+    
+    btnSidebareHeader.addEventListener('click', (event) =>{
+        console.log("load home!")
     });
+
+    // content button 
+    const btnSidebareContents = document.getElementsByClassName("sidebare-btn")
+
+    for (const btnSidebareContent in btnSidebareContents) {
+        
+    }
+
+
+
 });
