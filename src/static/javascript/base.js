35f0ea4e41d5367ae5f708773a05bf59b05e6814
kamgo-gpu <kamgoche@yahoo.com>
2021-01-06 19:31:38 +0100

    js for sidebare completed

--- src/static/javascript/base.js
+++ src/static/javascript/base.js
@@ -1,34 +1,46 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", () =>{
-
-        
-    const btnToggled = document.getElementById("button-toggle-id")
-    const sideContent = document.getElementById("sidebare-id")
-    const footerContent = document.getElementById("footer-content-id")
+document.addEventListener("DOMContentLoaded", () => {
 
     // toggeled side bar menu with JQuery
-    $("#button-toggle-id").click(function(){
+    $("#button-toggle-id").click(function () {
         $('.viewer-grid-content').toggleClass('viewer-grid-content-slided')
-        $('.sidebare').toggleClass('sidebare-hidden')        
-        // $('.footer-content').toggleClass('footer-content-hidden')
-        console.log("test")       
+        $('.sidebare').toggleClass('sidebare-hidden')
     });
 
-    // hauptButton menue sidebare
-    const btnSidebareHeader = document.getElementById("sidebare-btn-header-id")
-    
-    btnSidebareHeader.addEventListener('click', (event) =>{
-        console.log("load home!")
-    });
+    // content of sidebar
+    const sideContentItems = document.getElementsByClassName("side-items")
+    const sideBtnItems = document.querySelectorAll('.side-items .sidebare-btn-content .sidebare-btn-item')
+      
 
-    // content button 
-    const btnSidebareContents = document.getElementsByClassName("sidebare-btn")
+    for (const sideContentItem of sideContentItems) {
 
-    for (const btnSidebareContent in btnSidebareContents) {
-        
+        // make all header clickable
+        const itemHeader = sideContentItem.querySelector('.sidebare-btn-header')
+        const itemContent = sideContentItem.querySelector('.sidebare-btn-content')
+        const itemChevron = itemHeader.childNodes[5]
+        // add effek after click event on Header 
+        itemHeader.addEventListener('click', (event) =>{
+            // check if toggled 
+            if (itemHeader.classList.contains('toggled')){
+                // mask this sidebare-btn-content
+                itemHeader.classList.remove('toggled')
+                itemContent.classList.add('d-none')
+
+                // replace chevron up to down                
+                itemChevron.classList.remove('fa-chevron-up')
+                itemChevron.classList.add('fa-chevron-down')               
+
+            }
+            else{
+                // show this sidebare-btn-content 
+                itemHeader.classList.add('toggled')
+                itemContent.classList.remove('d-none')
+
+                // replace chevron down to up
+                itemChevron.classList.remove('fa-chevron-dow')
+                itemChevron.classList.add('fa-chevron-up')
+            }
+        })
     }
-
-
-
 });
