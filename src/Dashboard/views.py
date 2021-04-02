0d132ea10c04880029fc8a4105d48fbf3f541f50
romeolanry <kamgoche@yahoo.com>
2021-04-02 14:27:01 +0200

    create urls in to Dashboard

--- src/Dashboard/views.py
+++ src/Dashboard/views.py
@@ -1,22 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from kafka import  KafkaConsumer
-import json 
+import json
+import time
+import threading
 
-# Create your views here.
+# Create your views here
+
 
 def dashboard_view(request):
     context = {
-        'web_title': 'DeepViewer'
+        'web_title': 'DeepViewer',
+        'topic': "consumer"
     }
     return render(request,"home.html",context)
-
-def manager_consummer(request):
-    consumer = KafkaConsumer(
-        "jetson-topic",
-        bootstrap_servers=['192.168.0.2:9092'],
-        auto_offset_reset = 'earliest',
-    )
-
-    for msg in consumer:
-        print(f"Registered User {json.loads(msg.value)}")
-    return render(request, "home.html",consumer)
