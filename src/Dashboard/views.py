ed68667295ff36e8291f59534afff5905c3668f1
romeolanry <kamgoche@yahoo.com>
2021-04-02 10:49:47 +0200

    Commit with kafka consumer

--- src/Dashboard/views.py
+++ src/Dashboard/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from kafka import  KafkaConsumer
+import json 
 
 # Create your views here.
 
 def dashboard_view(request):
     context = {
-        'web_title': 'DashboarView'
+        'web_title': 'DeepViewer'
     }
     return render(request,"home.html",context)
+
+def manager_consummer(request):
+    consumer = KafkaConsumer(
+        "jetson-topic",
+        bootstrap_servers=['192.168.0.2:9092'],
+        auto_offset_reset = 'earliest',
+    )
+
+    for msg in consumer:
+        print(f"Registered User {json.loads(msg.value)}")
+    return render(request, "home.html",consumer)
