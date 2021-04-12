3b08fbc56c197a3d4cc2c2ef2a798ea26b73240f
romeolanry <kamgoche@yahoo.com>
2021-04-12 10:39:26 +0200

    Remvo redis configure into settings and add it into .env

--- src/deepstreamviewer/settings.py
+++ src/deepstreamviewer/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND':'channels_redis.core.RedisChannelLayer',
         'CONFIG' :{
-            'hosts':[('127.0.0.1', 6379)],
+            'hosts':[(config("REDIS_IP"), config("REDIS_PORT"))],
         }
     }
 }
